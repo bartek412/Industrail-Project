06b15b52de1133db14c85ae0277adb885837b7c2
AS - Adam Sztamborski <253234@edu.p.lodz.pl>
2025-02-02 21:36:30 +0100

    Some clean up

--- pupil_video_detect.py
+++ pupil_video_detect.py
@@ -6,6 +6,29 @@ import json
 import math
 from skimage.draw import polygon
 
+"""
+MAIN CONFIG
+"""
+###########################
+# which_videos_to_process = 'All'
+which_videos_to_process = 'all' # 'apriltag_1', 'apriltags_new' 'apriltags_p1' 'apriltags_p2' 'apriltags_p3'
+
+# filters
+apply_threshold_filter = True
+apply_shape_filter = True
+
+apply_registry_filter = False # this filter was decided to be no longer used (but could be in the future, thus it was left here)
+
+save_path_suffix = "_detection_pupil_threshold_shape2"
+save_video = True
+display_detection_results = False
+display_all_frames = True # if false it displays only frames with detections
+save2json_rejected_ids = False
+###########################
+
+
+
+
 
 
 # pupil apriltags detector config
@@ -17,13 +40,18 @@ REFINE_EDGES = 1
 DECODE_SHARPENING = 0.45
 DEBUG = 0
 
-# additional filters
+# filters variables - you can leave it as it is as it was empirically determined
 DECISION_MARGIN_THRESHOLD = 2.4 # for threshold filter (value 0-100)
-DISCRIMINANT_INERTIA = 5 # used only if bayesian filter enabled
+# you may ignore it - it is not a part of the project
+DISCRIMINANT_INERTIA = 5 # used only if registry filter enabled (but this filter was decided to be no longer used (but could be in the future, thus it was left here))
 
 print(f"""
         CONFIG:
-        Decision margin threshold: {DECISION_MARGIN_THRESHOLD}
+        apply_threshold_filter : {apply_threshold_filter},
+        Decision margin threshold: {DECISION_MARGIN_THRESHOLD},
+        apply_shape_filter : {apply_shape_filter},
+        save_video : {save_video},
+        save2json_rejected_ids : {save2json_rejected_ids},
         Discriminant inertia: {DISCRIMINANT_INERTIA}
 """)
 
@@ -47,8 +75,6 @@ def put_frame_number_and_video_title(original_frame, frame_number, num_of_frames
                 fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(0, 255, 0), thickness=2)
     return original_frame
 
-
-#TODO display all frames
 def display_detections(original_frame, detection_results, display_all_frames=False):
     if display_all_frames:
         cv2.imshow(f"AprilTags {len(detection_results)} - {video_src_path}", original_frame)
@@ -104,7 +130,7 @@ def count_idx(): # generator of indexes 0 to discriminant_inertia
         else:
             num = 0
 
-def bayes_filter(registry, last_frame_idx):
+def registry_filter(registry, last_frame_idx):
     num_of_ids = len(registry[last_frame_idx])
     flat_registry = []
     for r in registry:
@@ -124,16 +150,8 @@ def calcTriangleArea(len_a, len_b, len_c):
     s = (len_a + len_b + len_c) / 2
     return math.sqrt(s * (s - len_a) * (s - len_b) * (s - len_c))
 
-def create_results_mask(shape, detection_results):
-    mask = np.zeros(shape, dtype=np.uint8)
-    for result in detection_results:
-        xs = [corner[0] for corner in result.corners]
-        ys = [corner[1] for corner in result.corners]
-        yy_mask, xx_mask = polygon(ys, xs, shape)
-        mask[yy_mask, xx_mask] = 1
-    return mask
 # side2side_ratios estimated empirically : 0.505 (for medium angle, 0,39 for low angle), side_square_to_area_min_ratio ratios estimated empirically : 0.505 (for medium angle (0.5, 2.6), (0.4, 2.75) for low angle)
-def shape_filter(result, previous_results=None, shape=None, side2side_ratio = 0.39, diagonal2diagonal_ratio = 0.4, side_square_to_area_min_ratio=(0.4, 2.75), opposite_sides_min_ratio=0.65, disp_fp=False, original_frame=None):
+def shape_filter(result, previous_results=None, side2side_ratio = 0.39, diagonal2diagonal_ratio = 0.4, side_square_to_area_min_ratio=(0.4, 2.75), opposite_sides_min_ratio=0.65):
     (ptA, ptB, ptC, ptD) = result.corners
     # sides lengths
     len_ab = math.sqrt((ptA[0] - ptB[0]) ** 2 + (ptA[1] - ptB[1]) ** 2)
@@ -148,23 +166,14 @@ def shape_filter(result, previous_results=None, shape=None, side2side_ratio = 0.
 
     # check all sides 
     # 1. compare lengths of sides 2. compare diagonals 3. compare areas
-    """
-    1. Bayes approach -> track coordinates of many tags and their ids, then average coordinates
-    and classify to new detection based on closest tags from the past (so few registers)
 
-    2. Fusion of detectors -> average id based on different detectors (like open cv, apriltags of different versions)
-
-    3. Approximate solution - when you loose tag of given id and location still remember that and display it 
-    it may cover half of actual tag but algorithm still know more or less where it is.
-    
-    """
     # 1. We assume tag is somewhat a square, so its sides are expected to be more or less of equal length
     sides_lengths = [len_ab, len_bc, len_cd, len_da]
     if min(sides_lengths)/max(sides_lengths) < side2side_ratio:
         result.tag_id = str(result.tag_id)
         print(f"### 1.SHAPE FILTER ISSUE {result.tag_id}: min(sides_lengths)/max(sides_lengths) < side2side_ratio-> {min(sides_lengths)/max(sides_lengths)} < {side2side_ratio}")
         print(f"sides_lengths = {sides_lengths}")
-        return result
+        return result # True
         
     # 2. Diagonals comparison
     if min(len_ac/len_bd, len_bd/len_ac) < diagonal2diagonal_ratio:
@@ -188,17 +197,9 @@ def shape_filter(result, previous_results=None, shape=None, side2side_ratio = 0.
     # 5. Mask
     if previous_results == None:
         return result # False
-    """
-    result_mask = create_results_mask(shape, [result])
-    previous_results_mask = create_results_mask(shape, previous_results)
-    if np.sum(np.minimum(result_mask, previous_results_mask)) == 0:
-        result.tag_id = str(result.tag_id)
-        print(f"### 6.SHAPE FILTER ISSUE {result.tag_id}: np.sum(np.minimum(result_mask, previous_results_mask)) == 0 -> {np.sum(np.minimum(result_mask, previous_results_mask))} == 0 = {np.sum(np.minimum(result_mask, previous_results_mask)) == 0}")
-        return result # True
-    """
     return result # False
 
-def process_video(video_src_path, save_video=False, apply_threshold_filter=True, apply_bayes_filter=False, apply_shape_filter=False, display_detection_results=False):#, video_out_path, save=False):
+def process_video(video_src_path, save_video=False, apply_threshold_filter=True, apply_registry_filter=False, apply_shape_filter=False, display_detection_results=False):#, video_out_path, save=False):
     # Open video and check if video was opened successfully
     print(f"Opening: {video_src_path}")
     video = cv2.VideoCapture(video_src_path)
@@ -227,7 +228,7 @@ def process_video(video_src_path, save_video=False, apply_threshold_filter=True,
     frames_counter = 0
     num_of_frames_in_video = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    if apply_bayes_filter:
+    if apply_registry_filter:
         # initialize - fill ids_raw_registry
         ids_raw_registry = [':)' for d in range(DISCRIMINANT_INERTIA)] # raw detected ids (no swaps)
         idx_counter = count_idx()
@@ -248,9 +249,9 @@ def process_video(video_src_path, save_video=False, apply_threshold_filter=True,
             print(f'progress: {stats["frames_numbers"]} / {num_of_frames_in_video} ({stats["frames_numbers"] / num_of_frames_in_video * 100:.2f} %)')
             
             if apply_shape_filter:
-                detection_results = [shape_filter(result=r, previous_results=previous_results, shape=image.shape) for r in detection_results]
+                detection_results = [shape_filter(result=r, previous_results=previous_results) for r in detection_results]
 
-            detected_id = [r.tag_id for r in detection_results] # raw id coming from detecor (it might be a result of hallucination) - so next frame detection result
+            detected_id = [r.tag_id for r in detection_results] # raw id coming from detecor (may contain invalid detections) - so next frame detection result
             ids_raw_registry[i] = detected_id # load n frames to fill registry
             print(f"ids_raw_registry[{reg_idx}] << {detected_id}")
 
@@ -289,15 +290,15 @@ def process_video(video_src_path, save_video=False, apply_threshold_filter=True,
             detection_results = [threshold_filter(result=r) for r in detection_results]
 
         if apply_shape_filter:
-            detection_results = [shape_filter(result=r, previous_results=previous_results, shape=image.shape) for r in detection_results]
+            detection_results = [shape_filter(result=r, previous_results=previous_results) for r in detection_results]
 
-        if apply_bayes_filter and len(detection_results) == 1:
+        if apply_registry_filter and len(detection_results) == 1:
             detected_id = [r.tag_id for r in detection_results]
             reg_idx = next(idx_counter)
             ids_raw_registry[reg_idx] = detected_id
             print(f"ids_raw_registry[{reg_idx}] << {detected_id}")
-            swap_id = bayes_filter(registry=ids_raw_registry, last_frame_idx=reg_idx)[0]
-            print(f"Bayesian_filter swap id {detection_results[0].tag_id} with {swap_id}")
+            swap_id = registry_filter(registry=ids_raw_registry, last_frame_idx=reg_idx)[0]
+            print(f"registryian_filter swap id {detection_results[0].tag_id} with {swap_id}")
             detection_results[0].tag_id = swap_id
 
         if save_video or display_detection_results:
@@ -307,7 +308,7 @@ def process_video(video_src_path, save_video=False, apply_threshold_filter=True,
             if save_video:
                 video_saver.write(original_frame)
             if display_detection_results:
-                display_detections(original_frame=original_frame, detection_results=detection_results, display_all_frames=True)
+                display_detections(original_frame=original_frame, detection_results=detection_results, display_all_frames=display_all_frames)
 
 
         for r in detection_results:
@@ -331,15 +332,6 @@ def process_video(video_src_path, save_video=False, apply_threshold_filter=True,
     return
 
 if __name__ == "__main__":
-#    which_videos_to_process = 'All'
-    which_videos_to_process = 'all' # 'apriltag_1', 'apriltags_new' 'apriltags_p1' 'apriltags_p2' 'apriltags_p3'
-    save_path_suffix = "_detection_pupil_threshold_shape2"
-    save_video = True
-    save2json_rejected_ids = False
-    display_detection_results = False
-    apply_threshold_filter = True
-    apply_bayes_filter = False
-    apply_shape_filter = True
 
     if which_videos_to_process == 'all':
         videos_dir = [v for v in os.listdir(os.getcwd()+'/videos') if v[-4:] == ".mp4"]
@@ -347,11 +339,11 @@ if __name__ == "__main__":
         for name in videos_dir:
             video_src_path = f'videos/{name}'
             video_out_path = f'videos{save_path_suffix}/{name[:-4]}{save_path_suffix}.mp4'
-            process_video(video_src_path, apply_threshold_filter=apply_threshold_filter, apply_bayes_filter=apply_bayes_filter, apply_shape_filter=apply_shape_filter, save_video=save_video, display_detection_results=display_detection_results)
+            process_video(video_src_path, apply_threshold_filter=apply_threshold_filter, apply_registry_filter=apply_registry_filter, apply_shape_filter=apply_shape_filter, save_video=save_video, display_detection_results=display_detection_results)
 
     else:
         name = which_videos_to_process
         video_src_path = f'videos/{name}.mp4'
         video_out_path = f'videos{save_path_suffix}/{name}{save_path_suffix}.mp4'
-        process_video(video_src_path, apply_threshold_filter=apply_threshold_filter, apply_bayes_filter=apply_bayes_filter, apply_shape_filter=apply_shape_filter, save_video=save_video, display_detection_results=display_detection_results)
+        process_video(video_src_path, apply_threshold_filter=apply_threshold_filter, apply_registry_filter=apply_registry_filter, apply_shape_filter=apply_shape_filter, save_video=save_video, display_detection_results=display_detection_results)
 
